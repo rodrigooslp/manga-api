3387ff24c61cf23b45741494dc88bad92c6b98e2
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-06-14 17:24:26 -0400

    Ajuste na função da rota

--- routes/download.js
+++ routes/download.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 var MangaController = require('../controllers/manga')
 
-router.get('/', async function (req, res) {
+router.get('/', async (req, res) => {
   const { nome, capitulo } = req.query
   const result = await MangaController.download(nome, capitulo)
   res.send(result)
